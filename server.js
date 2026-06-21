7852c7627ce6fe4c376f5ac88f87e6b0c8cc646f
Aiman2027 <aimannishat78692@gmail.com>
2026-06-22 03:35:24 +0530

    fix: export app for vercel

--- server.js
+++ server.js
@@ -18,3 +18,4 @@ app.get("/", (req, res) => res.send("Expense Tracker API running"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
